b58405ea6398aabdbc70984d5c20cde2ce5b7967
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-07-11 20:16:16 -0300

    feat: add TypeORM repository import for user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,16 @@ import { Module } from "@nestjs/common";
 import UserController from "./user.controller";
 import UserRepository from "./user.repository";
 import { IsEmailUniqueValidator } from "./validations/isEmailUnique.validator";
+import UserService from "./user.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
+   imports: [TypeOrmModule.forFeature([UserRepository])],
    controllers: [UserController],
-   providers: [UserRepository, IsEmailUniqueValidator]
+   providers: [
+      UserService,
+      UserRepository, 
+      IsEmailUniqueValidator
+   ]
 })
 export class UserModule {}
