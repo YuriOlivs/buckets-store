a3b6ae1cbf26239fa877c79c5230010ae1715270
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-07-11 17:14:39 -0300

    feat: add entity annotations to user.entity

--- src/team/team.entity.ts
+++ src/team/team.entity.ts
@@ -1,6 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'teams' })
 export default class TeamEntity {
+   @PrimaryGeneratedColumn('uuid')
    private id: string;
+
+   @Column({ name: 'name', length: 100, nullable: false })
    private name: string;
+
+   @Column({ name: 'city', length: 100, nullable: false })
    private city: string;
 
    constructor(id: string, name: string, city: string) {
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,39 +1,95 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
 export default class UserEntity {
-   private id: string;
-   private name: string;
-   private lastName: string;
-   private email: string;
-   private password: string;
-   private birthDate: Date;
+  @PrimaryGeneratedColumn('uuid')
+  private id: string;
 
-   constructor(
-      id: string, 
-      name: string, 
-      lastName: string, 
-      email: string, 
-      password: string, 
-      birthDate: Date
-   ) {
-      this.id = id;
-      this.name = name;
-      this.lastName = lastName;
-      this.email = email;
-      this.password = password;
-      this.birthDate = birthDate;
-   }
+  @Column({ name: 'name', length: 100, nullable: false })
+  private name: string;
 
-   get getId() { return this.id; }
-   get getName() { return this.name; }
-   get getLastName() { return this.lastName; }
-   get fullName() { return `${this.name} ${this.lastName}`; }
-   get getEmail() { return this.email; }
-   get getPassword() { return this.password; }
-   get getBirthDate() { return this.birthDate; }
+  @Column({ name: 'lastName', length: 100, nullable: false })
+  private lastName: string;
 
-   //setId(id: string) { this.id = id; }
-   setName(name: string) { this.name = name; }
-   setLastName(lastName: string) { this.lastName = lastName; }
-   setEmail(email: string) { this.email = email; }
-   setPassword(password: string) { this.password = password; }
-   setBirthDate(birthDate: Date) { this.birthDate = birthDate; }
+  @Column({ name: 'email', length: 80, nullable: false })
+  private email: string;
+
+  @Column({ name: 'password', length: 255, nullable: false })
+  private password: string;
+
+  @Column({ name: 'birthDate', nullable: false })
+  private birthDate: Date;
+  
+  @CreateDateColumn({ name: 'created_at' })
+  private createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  private updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  private deletedAt: string;
+
+  constructor(
+    id: string,
+    name: string,
+    lastName: string,
+    email: string,
+    password: string,
+    birthDate: Date,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.birthDate = birthDate;
+  }
+
+  get getId() {
+    return this.id;
+  }
+  get getName() {
+    return this.name;
+  }
+  get getLastName() {
+    return this.lastName;
+  }
+  get fullName() {
+    return `${this.name} ${this.lastName}`;
+  }
+  get getEmail() {
+    return this.email;
+  }
+  get getPassword() {
+    return this.password;
+  }
+  get getBirthDate() {
+    return this.birthDate;
+  }
+  get getCreatedAt() {
+    return this.createdAt;
+  }
+  get getUpdatedAt() {
+    return this.updatedAt;
+  }
+  get getDeletedAt() {
+    return this.deletedAt;
+  }
+
+  //setId(id: string) { this.id = id; }
+  setName(name: string) {
+    this.name = name;
+  }
+  setLastName(lastName: string) {
+    this.lastName = lastName;
+  }
+  setEmail(email: string) {
+    this.email = email;
+  }
+  setPassword(password: string) {
+    this.password = password;
+  }
+  setBirthDate(birthDate: Date) {
+    this.birthDate = birthDate;
+  }
 }
