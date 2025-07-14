bbfc3016309e6a764faf5e02b8922497ce3c48e0
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-07-14 19:42:26 -0300

    feat: update script to add logo column to team and add images table

--- create_tables_script.sql
+++ create_tables_script.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS public.users (
   created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
   updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
   deleted_at TIMESTAMP WITHOUT TIME ZONE,
+
   CONSTRAINT pk_users PRIMARY KEY (id)
 );
 
@@ -21,10 +22,17 @@ CREATE TABLE IF NOT EXISTS public.teams (
   id uuid NOT NULL DEFAULT uuid_generate_v4(),
   name character varying(100) COLLATE pg_catalog."default" NOT NULL,
   city character varying(100) COLLATE pg_catalog."default" NOT NULL,
+  logo_id UUID NOT NULL,
   created_at timestamp without time zone NOT NULL DEFAULT now(),
   updated_at timestamp without time zone NOT NULL DEFAULT now(),
   deleted_at timestamp without time zone,
-  CONSTRAINT PK_teams PRIMARY KEY (id)
+
+  CONSTRAINT PK_teams PRIMARY KEY (id),
+
+  CONSTRAINT fk_teams_logo FOREIGN KEY (logo_id)
+    REFERENCES public.images (id)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
 );
 
 ALTER TABLE IF EXISTS public.teams OWNER TO root;
@@ -41,7 +49,9 @@ CREATE TABLE IF NOT EXISTS public.products (
   created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
   updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
   deleted_at TIMESTAMP WITHOUT TIME ZONE,
+
   CONSTRAINT pk_products PRIMARY KEY (id),
+
   CONSTRAINT fk_products_team FOREIGN KEY (team_id)
     REFERENCES public.teams (id)
     ON UPDATE CASCADE
@@ -51,16 +61,30 @@ CREATE TABLE IF NOT EXISTS public.products (
 ALTER TABLE IF EXISTS public.products OWNER TO root;
 
 -- IMAGES
-CREATE TABLE IF NOT EXISTS public.product_images (
+CREATE TABLE IF NOT EXISTS public.images (
   id UUID NOT NULL DEFAULT uuid_generate_v4(),
   url VARCHAR(255) NOT NULL,
   description VARCHAR(255),
-  product_id UUID NOT NULL,
-  CONSTRAINT pk_product_images PRIMARY KEY (id),
-  CONSTRAINT fk_product_images_product FOREIGN KEY (product_id)
+
+  product_id UUID,
+  team_id UUID,
+
+  CONSTRAINT pk_images PRIMARY KEY (id),
+
+  CONSTRAINT fk_images_product FOREIGN KEY (product_id)
     REFERENCES public.products (id)
     ON UPDATE CASCADE
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
+
+  CONSTRAINT fk_images_team FOREIGN KEY (team_id)
+    REFERENCES public.teams (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+  CONSTRAINT chk_only_one_parent CHECK (
+    (product_id IS NOT NULL AND team_id IS NULL) OR
+    (product_id IS NULL AND team_id IS NOT NULL)
+  )
 );
 
-ALTER TABLE IF EXISTS public.product_images OWNER TO root;
+ALTER TABLE IF EXISTS public.images OWNER TO root;
