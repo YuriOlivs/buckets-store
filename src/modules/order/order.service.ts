e88719165e9580d07160d988547b04cb0fd8749b
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-07-29 12:28:52 -0300

    feat: update service constructor parameters and comment createOrder implementation

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,30 +1,59 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { OrderCreateDTO } from './dto/OrderCreate.dto';
 import { OrderUpdateDTO } from './dto/OrderUpdate.dto';
 import ProductEntity from '../product/product.entity';
 import OrderRepository from './order.repository';
 import ProductService from '../product/product.service';
 import { OrderItemEntity } from '../order-item/order-item.entity';
+import { OrderEntity } from './order.entity';
+import UserService from '../user/user.service';
+import { OrderStatusService } from '../order-status/order-status.service';
+import { OrderStatusEntity } from '../order-status/order-status.entity';
+import { OrderItemService } from '../order-item/order-item.service';
 
 @Injectable()
 export default class OrderService {
   constructor(
     private repo: OrderRepository,
-    private productService: ProductService
+    private productService: ProductService,
+    private userService: UserService,
+    private orderStatusService: OrderStatusService,
+    private orderItemService: OrderItemService
   ) {}
 
-  createOrder(dto: OrderCreateDTO) {
-    const products: ProductEntity[] = [];
-    const orderItems: OrderItemEntity[] = [];
+  async createOrder(dto: OrderCreateDTO) {
+    // const products: ProductEntity[] = [];
+    // const orderItems: OrderItemEntity[] = [];
+    // let totalValue = 0;
     
-    dto.products.forEach(async item => {
-      const productFound = await this.productService.getProductById(item.product.id);
-      if(!productFound) throw new NotFoundException('Product not found');
+    // const userFound = await this.userService.getUserById(dto.user);
+    // if(!userFound) throw new NotFoundException('User not found');
 
-      products.push(productFound);
-    });
+    // dto.products.forEach(async item => {
+    //   const productFound = await this.productService.getProductById(item.product.id);
+    //   if(!productFound) throw new NotFoundException('Product not found');
 
+    //   const productValue = productFound.price * item.quantity;
+    //   totalValue += productValue;
+
+    //   products.push(productFound);
+    // });
+
+    // //creating order
+    // const order = new OrderEntity(totalValue, userFound);
+    // const orderCreated = await this.repo.save(order);
+
+    // if (!orderCreated) { throw new InternalServerErrorException('Error creating order'); }
+
+    // //creating order status
+    // const orderStatus = new OrderStatusEntity(orderCreated, 'Waiting payment', new Date());
+    // const orderStatusCreated = await this.orderStatusService.save(orderStatus);
+
+    // if (!orderStatusCreated) { throw new InternalServerErrorException('Error creating order status'); }
+
+    // //creating order item
     
+    // return orderCreated;
   }
 
   findAllOrders() {
