10b3c49f3173d6e062da97e8a5ea9fc10df0009e
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-07-29 10:17:26 -0300

    feat: add order item entity's initial structure

--- src/modules/order-item/order-item.entity.ts
+++ src/modules/order-item/order-item.entity.ts
@@ -1,1 +1,26 @@
-export class OrderItem {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: 'order_items' })
+export class OrderItem {
+   @PrimaryGeneratedColumn('uuid')
+   id: string;
+
+   orderId: string;
+
+   productId: string;
+
+   @Column({ name: 'quantity', type: 'int', nullable: false })
+   quantity: number;
+
+   @Column({ name: 'sale_price', type: 'numeric', precision: 10, scale: 2, nullable: false })
+   salePrice: number;
+
+   @CreateDateColumn({ name: 'created_at' })
+   createdAt: Date;
+
+   @UpdateDateColumn({ name: 'updated_at' })
+   updatedAt: Date;
+
+   @DeleteDateColumn({ name: 'deleted_at' })
+   deletedAt: Date;
+}
