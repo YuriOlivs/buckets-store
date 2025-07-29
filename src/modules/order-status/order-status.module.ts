b1ad9c8f693c73173d16334618e5d0acfc1a885f
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-07-29 11:16:45 -0300

    feat: add order item and order status repository files

--- src/modules/order-status/order-status.module.ts
+++ src/modules/order-status/order-status.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderStatusService } from './order-status.service';
 import { OrderStatusController } from './order-status.controller';
+import OrderStatusRepository from './order-status.repository';
 
 @Module({
   controllers: [OrderStatusController],
-  providers: [OrderStatusService],
+  providers: [OrderStatusService, OrderStatusRepository],
 })
 export class OrderStatusModule {}
