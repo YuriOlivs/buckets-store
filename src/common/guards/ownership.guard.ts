ebf0ab8cf5873358e92b439d940ab9c6082f38a0
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-08-12 00:04:16 -0300

    fix: ownership guard

--- src/common/guards/ownership.guard.ts
+++ src/common/guards/ownership.guard.ts
@@ -1,15 +1,18 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { STRINGS } from '../strings/global.strings';
+import RequestWithUser from 'src/modules/auth/dto/req-with-user.dto';
 
 @Injectable()
 export class OwnershipGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
     const userIdParam = request.params.id;
-    const userIdToken = request.user.id;
+    const userIdToken = request.user.sub;
+
+    console.log(userIdParam, userIdToken);
 
     if (userIdParam !== userIdToken) {
       throw new ForbiddenException(STRINGS.notAuthorized());
